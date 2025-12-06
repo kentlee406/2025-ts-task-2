da66a1be3dd170427b3e77ed23f517ffa462da4a
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2025-12-07 01:19:45 +0800

    vite.config.ts 更新

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/2025-ts-task-2/', // Github Pages 子路徑
+  base: '/2025-ts-task-2/',
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // @ -> src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  plugins: [vue()],
 })
