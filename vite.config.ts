c4406fa11b7202a59275b85e98d384f2881c4c6c
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2025-12-06 17:28:39 +0800

    Revision 11

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   base: '/2025-ts-task-2/', // Github Pages
+  build: {
+    // 不要額外修改 rollupOptions，避免錯誤
+    outDir: 'dist', // 預設即可，或改成你想的資料夾
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // 這行很重要
