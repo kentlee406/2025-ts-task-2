83b7a7c86aa970cbf20d0b1812ab92e37e95963e
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2025-12-07 01:34:43 +0800

    退版本

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,20 @@
-import Dashboard from '@/components/Dashboard.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard,
-      children: [
-        {
-          path: '/product-management',
-          name: 'product-management',
-          component: () => import('@/views/ProductManagement.vue'),
-        },
-        {
-          path: '/order-management',
-          name: 'order-management',
-          component: () => import('@/views/OrderManagement.vue'),
-        },
-      ],
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/Login.vue'),
-    },
-  ],
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-
-    return {
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    }
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory('/2025-ts-task-2/'),
+  routes,
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,15 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
-
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/2025-ts-task-2/', // ← 注意這裡
-  plugins: [vue(), vueDevTools()],
+  base: '/2025-ts-task-2/',
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: resolve(__dirname, 'dist'),
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-        silenceDeprecations: ['mixed-decls', 'import', 'color-functions', 'global-builtin'],
-        verbose: false,
-      },
-    },
-  },
+
+  plugins: [vue()],
 })
